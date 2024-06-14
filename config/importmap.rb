a54f16e4f6d3a510d754d3622060f0d3b86647c5
ger619 <draker74@gmail.com>
2024-06-14 14:08:59 +0300

    add all ammendments

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
 pin "chart.js" # @4.4.3
 pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+
